76b38600cb1bd1e4a3b1fe51fb6340904736695b
Qingyi Chang <esmidth@163.com>
2017-06-03 22:15:23 +0800

    2017.6.2.2 Finished Practice 18

--- 18.py
+++ 18.py
@@ -75,25 +75,51 @@ def ex_4():  # delete small objects
         mask = ndi.gaussian_filter(mask, sigma=l / (4. * n))
         return mask > mask.mean()
 
-    data = micro_structure(l=128)
-    dst = morphology.remove_small_objects(data, min_size=300, connectivity=1)
+    local_data = micro_structure(l=128)
+    dst = morphology.remove_small_objects(local_data, min_size=300, connectivity=1)
 
     fig, (ax1, ax2) = plt.subplots(1, 2)
-    ax1.imshow(data, plt.cm.gray, interpolation='nearest')
+    ax1.imshow(local_data, plt.cm.gray, interpolation='nearest')
     ax2.imshow(dst, plt.cm.gray, interpolation='nearest')
 
     fig.tight_layout()
     plt.show()
 
 
-def ex_5():
+def ex_5():  # 阈值分割，闭运算，连通区域标记，删除小区块，分色显示
+    # 加载图片，并裁剪
     image = data.coins()[50:-50, 50:-50]
 
-    thresh = filters.threshold_otsu(image)
+    thresh = filters.threshold_otsu(image)  # 阈值分割
     bw = morphology.closing(image > thresh, morphology.square(3))
+    # 闭运算
 
-    cleared = bw.copy()
+    cleared = bw.copy()  # 复制
+    segmentation.clear_border(cleared)  # 清除与边界相连的目标物
+
+    label_image = measure.label(cleared)  # 连通区域标记
+    borders = np.logical_xor(bw, cleared)  # 异或
+    label_image[borders] = -1
+    image_label_overlay = color.label2rgb(label_image, image=image)
+    # 不同标记用不同颜色显示
+
+    fig, (ax0, ax1) = plt.subplots(1, 2)
+    ax0.imshow(cleared, plt.cm.gray)
+    ax1.imshow(image_label_overlay)
+
+    for region in measure.regionprops(label_image):  # 循环得到每一个连通区域属性集
+
+        # 忽略小区域
+        if region.area < 100:
+            continue
+        # 绘制外包矩形
+        minr, minc, maxr, maxc = region.bbox
+        rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr, fill=False, edgecolor='red', linewidth=2)
+        ax1.add_patch(rect)
+
+    fig.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
-    ex_4()
+    ex_5()
